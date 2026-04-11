b145994dc149f0c63eb31b3583ad72aaef777054
londospark <londospark@users.noreply.github.com>
2026-04-11 05:31:08 +0100

    Stub second query so that we can return a list of food items

--- crates/gateway/src/lib.rs
+++ crates/gateway/src/lib.rs
@@ -1,12 +1,31 @@
 use anyhow::{Ok, Result};
-use async_graphql::Object;
+use async_graphql::{Object, SimpleObject};
 
 pub struct Query;
+
+#[derive(SimpleObject)]
+pub struct Food {
+    name: String,
+    qty: i32,
+}
 
 #[Object]
 impl Query {
     async fn health(&self) -> Result<String> {
         Ok(format!("ok"))
+    }
+
+    async fn list_food(&self) -> Result<Vec<Food>> {
+        Ok(vec![
+            Food {
+                name: "Pizza".to_string(),
+                qty: 10,
+            },
+            Food {
+                name: "Burger".to_string(),
+                qty: 5,
+            },
+        ])
     }
 }
 
@@ -20,5 +39,15 @@ mod tests {
         let schema = Schema::new(Query, EmptyMutation, EmptySubscription);
         let res = schema.execute("{ health }").await;
         assert_eq!(res.data.to_string(), "{health: \"ok\"}");
+    }
+
+    #[tokio::test]
+    async fn list_food_returns_a_list_of_food() {
+        let schema = Schema::new(Query, EmptyMutation, EmptySubscription);
+        let res = schema.execute("{ listFood { name, qty } }").await;
+        assert_eq!(
+            res.data.to_string(),
+            "{listFood: [{name: \"Pizza\", qty: 10}, {name: \"Burger\", qty: 5}]}"
+        );
     }
 }
