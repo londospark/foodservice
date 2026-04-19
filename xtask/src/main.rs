9e0093d64ac87e00a2f4ca509badf67b094d28d9
Londo <ridecar2@gmail.com>
2026-04-19 07:47:06 +0100

    Some helpers for infrastructure

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -17,10 +17,14 @@ enum Commands {
     ///     This command will install all the necessary dependencies for the project using Cargo.
     Deps,
     ///
-    /// Setup the database
+    /// Setup docker compose
     ///     
-    ///     This command will set up the database for the project, including creating necessary tables and seeding initial data.
-    DbSetup,
+    ///     This command will set up the Docker Compose environment for the project, allowing you to easily manage and run the various services that make up the foodservice microservices infrastructure.
+    ComposeUp,
+    ///Run tests
+    ///     
+    ///     This command will run all the tests for the project, ensuring that everything is working as expected.
+    Test,
 }
 
 fn main() {
@@ -41,6 +45,33 @@ fn main() {
             }
             println!("Dependencies installed successfully!");
         }
-        Commands::DbSetup => todo!(),
+        Commands::Test => {
+            println!("Running tests...");
+            let status = std::process::Command::new("cargo")
+                .args(["test", "--no-fail-fast", "--color", "always", "--workspace"])
+                .status()
+                .expect("Failed to run tests");
+
+            if !status.success() {
+                eprintln!("Tests failed. Please check the error messages above.");
+                std::process::exit(1);
+            }
+            println!("All tests passed successfully!");
+        }
+        Commands::ComposeUp => {
+            println!("Setting up Docker Compose environment...");
+            let status = std::process::Command::new("docker-compose")
+                .args(["up", "-d"])
+                .status()
+                .expect("Failed to set up Docker Compose environment");
+
+            if !status.success() {
+                eprintln!(
+                    "Failed to set up Docker Compose environment. Please check the error messages above."
+                );
+                std::process::exit(1);
+            }
+            println!("Docker Compose environment set up successfully!");
+        }
     }
 }
